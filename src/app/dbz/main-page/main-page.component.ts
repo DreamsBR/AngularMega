75a99f334cc868f6fe5328c2c9891591761f832d
MarcosBR <marcosbr301@gmail.com>
2021-02-05 00:33:21 -0800

    ok

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -11,20 +11,43 @@ interface Personajes{
   templateUrl: './main-page.component.html',
 })
 export class MainPageComponent  {
-
+  personajes : Personajes[] = [
+    {
+      nombre : 'Goku SJ3',
+      poder : 1900
+    },
+    {
+      nombre : 'Vegeta SJ3',
+      poder : 1500
+    },
+    {
+      nombre : 'Gohan SJ3',
+      poder : 1000
+    }
+  ]
   constructor() { }
 
   ngOnInit(): void {
   }
 
   nuevo : Personajes ={
-    nombre : 'Truns',
-    poder : 1000
+    nombre : '',
+    poder : 0
   }
 
-  agregar (){
 
-    console.log(this.nuevo)
+  
+  agregar (){
+    if (this.nuevo.nombre.trim().length === 0){
+      return ;
+    }
+    this.personajes.push(this.nuevo);
+    this.nuevo = {
+      nombre : '',
+      poder : 0
+    }
+    
+    console.table(this.personajes)
   }
 
   cambiarNombre(event : any){
